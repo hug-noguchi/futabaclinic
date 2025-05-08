a317c941edf91c57e3a2b8ca73cd38749f97bf09
keito noguchi <noguchi@hug-inc.co.jp>
2025-05-08 19:35:16 +0900

    サイドバー、フッター取得内容修正

--- news/wp-content/themes/futabaclinic/single-kitasenju.php
+++ news/wp-content/themes/futabaclinic/single-kitasenju.php
@@ -27,17 +27,16 @@
 					  endwhile;
 					endif;
 					?>
-
 				</section>
 				<div class="navigation">
 					<span class="previous"><?php previous_post_link(' %link', '&lt;&nbsp;前の記事へ'); ?></span>
 					<span class="next"><?php next_post_link('%link ', '次の記事へ&nbsp;&gt;'); ?></span>
 				</div>
 			</div>
-			<?php include($_SERVER['DOCUMENT_ROOT'] . '/include/side_kitasenju.php'); ?>
-			</div>
+			<?php include($_SERVER['DOCUMENT_ROOT'] . '/template/sidebar.php'); ?>
+		</div>
   </article>
-	<?php include($_SERVER['DOCUMENT_ROOT'] . '/include/footer_kitasenju.php'); ?>
+	<?php include($_SERVER['DOCUMENT_ROOT'] . '/template/footer.php'); ?>
 	<?php wp_footer(); ?>
 </body>
 </html>
