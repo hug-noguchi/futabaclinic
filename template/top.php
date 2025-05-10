ad61c6483dc60f6f5412f54b3b6888ecccc7a009
keito noguchi <noguchi@hug-inc.co.jp>
2025-05-10 14:23:17 +0900

    分院案内「埼玉院」に地域名「五霞町」追加

--- template/top.php
+++ template/top.php
@@ -295,6 +295,7 @@
                         <li>白岡市</li>
                         <li>蓮田市</li>
                         <li>春日部市</li>
+                        <li>五霞町</li>
                       </ul>
                       <p class="txt_13">の患者様はこちら</p>
                       <p class="txt_14">
